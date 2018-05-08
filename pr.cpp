a2e43c81fd9d117acd58f329d19fe8f9b6f8cbac
trushan <36843932+trushan@users.noreply.github.com>
2018-05-08 15:17:53 +0300

    Add files via upload

---
+++ pr.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+using namespace std;
+
+string name;
+
+int main()
+{
+	//input
+	cout << "Enter your name >> ";
+	cin >> name;
+	cout << endl;
+	
+	//output
+	cout << "Hello, " << name << "!";
+	
+	return 0;
+}
